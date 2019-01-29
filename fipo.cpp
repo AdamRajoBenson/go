fb9bdc4a0ab527ea76048184bd95b7ae67c48015
AdamRajoBenson <42169883+AdamRajoBenson@users.noreply.github.com>
2019-01-29 22:22:09 +0530

    Create fipo.cpp

---
+++ fipo.cpp
@@ -0,0 +1,16 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+int a[100],n;
+cin>>n;
+for(int i=0;i<n;i++)
+{
+cin>>a[i];
+}
+for(int i=0;i<n;i++)
+{
+cout<<a[i]<<" "<<i<<"\n";
+}
+getch();
+}
